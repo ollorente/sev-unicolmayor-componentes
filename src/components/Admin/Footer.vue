c3eb93d803b5645d700c534522e15c364a2d02a3
Óscar Llorente <llorente_oscar@hotmail.com>
2023-08-11 00:50:34 -0500

    Creando archivo

---
+++ src/components/Admin/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="w-100 p-3">
+    <div class="container mx-auto">
+      <div class="flex flex-col md:flex-row text-center">
+        <span class="">Subdirección de Educación Virtual</span>
+        <span class="px-2">&copy; 2022 - {{ new Date().getFullYear() }}</span>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<style scoped></style>
---
+++ src/views/Admin/Program/programNew.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import AdminLayout from "./../../../layouts/admin.vue"
+import UIHead from "./../../../components/Client/Head.vue"
+</script>
+
+<template>
+  <AdminLayout>
+    <UIHead back backUrl="/admin/programas">
+      Agregar programa
+    </UIHead>
+
+    <div class="card border-0 shadow my-3">
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Text</p>
+      </div>
+    </div>
+  </AdminLayout>
+</template>
+
+<style scoped></style>

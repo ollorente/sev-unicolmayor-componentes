e51432c174321595f78262afda779e4ad90d4383
Óscar Llorente <llorente_oscar@hotmail.com>
2023-08-10 23:27:52 -0500

    Agregando estructura

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,6 +1,9 @@
 import { v4 } from "uuid"
 export const DATE = new Date().toISOString()
 
+export const FB_APP_ID = "etP9QbnP2xhmoDb5C2mF"
+export const FB_APP_NAME = "moodle"
+
 // = INTERFACES =====================================================
 
 interface IBase {
@@ -14,14 +17,25 @@ interface IBase {
 export interface ICheck {
   check: boolean
   createdAt: string
+  updatedAt?: string
   userId: string
 }
 
 export enum ERole { "admin", "designer", "developer", "guest", "superuser", "teacher" }
 
+export interface IFaculty extends IBase {
+  name: string
+}
+
 export interface IProgram extends IBase {
+  facultyId: string
+  name: string
+}
+
+export interface IResource extends IBase {
   approved: ICheck
   bibliography: string
+  component: string
   content: string
   elaborated: ICheck
   facultyId: string
@@ -36,6 +50,11 @@ export interface IProgram extends IBase {
   unit: string
 }
 
+export interface IRole extends IBase {
+  description?: string
+  name: string
+}
+
 export interface IUser extends IBase {
   email: string
   image: string
@@ -47,37 +66,107 @@ export interface IUser extends IBase {
 
 // = MODELS =========================================================
 
-export const Program = (db: IProgram) => {
+export const Check = (db: ICheck) => {
   return {
-    approved: Object(db.approved ? db.approved : {}),
-    bibliography: String(db.bibliography ? db.bibliography : ""),
-    content: String(db.content ? db.content : ""),
-    createdAt: String(db.createdAt ? db.createdAt : ""),
-    elaborated: Object(db.elaborated ? db.elaborated : {}),
+    check: String(db.check === true ? true : false),
+    createdAt: String(db.createdAt ? db.createdAt : DATE),
+    updatedAt: String(db.updatedAt ? db.updatedAt : DATE),
+    userId: String(db.userId ? db.userId : ""),
+  }
+}
+
+export const Faculty = (db: IFaculty) => {
+  return {
+    createdAt: String(db.createdAt ? db.createdAt : DATE),
     id: String(db.id ? db.id : v4()),
-    facultyId: String(db.facultyId ? db.facultyId : ""),
-    index: String(db.index ? db.index : ""),
-    integrated: Object(db.integrated ? db.integrated : {}),
-    introduction: String(db.introduction ? db.introduction : ""),
     isActive: Boolean(db.isActive === true ? true : false),
     isLock: Boolean(db.isLock === true ? true : false),
-    programId: String(db.programId ? db.programId : ""),
-    revised: Object(db.revised ? db.revised : {}),
-    summary: String(db.summary ? db.summary : ""),
-    teacher: String(db.teacher ? db.teacher : ""),
-    title: String(db.title ? db.title : ""),
-    unit: String(db.unit ? db.unit : ""),
-    updatedAt: String(db.updatedAt ? db.updatedAt : ""),
+    name: String(db.name.trim()),
+    updatedAt: String(db.updatedAt ? db.updatedAt : DATE),
+  }
+}
+
+export const RefFaculty = (db: IFaculty) => {
+  return {
+    id: String(db.id ? db.id : v4()),
+    isActive: Boolean(db.isActive === true ? true : false),
+    name: String(db.name.trim()),
+  }
+}
+
+export const Program = (db: IProgram) => {
+  return {
+    createdAt: String(db.createdAt ? db.createdAt : DATE),
+    facultyId: String(db.facultyId ? db.facultyId.trim() : ""),
+    id: String(db.id ? db.id : v4()),
+    isActive: Boolean(db.isActive === true ? true : false),
+    isLock: Boolean(db.isLock === true ? true : false),
+    name: String(db.name.trim()),
+    updatedAt: String(db.updatedAt ? db.updatedAt : DATE),
   }
 }
 
 export const RefProgram = (db: IProgram) => {
   return {
+    facultyId: Object(db.facultyId ? db.facultyId : {}),
     id: String(db.id ? db.id : v4()),
     isActive: Boolean(db.isActive === true ? true : false),
-    teacher: String(db.teacher ? db.teacher : ""),
-    title: String(db.title ? db.title : ""),
-    unit: String(db.unit ? db.unit : ""),
+    name: String(db.name.trim()),
+  }
+}
+
+export const Resource = (db: IResource) => {
+  return {
+    approved: Object(db.approved ? db.approved : {}),
+    bibliography: String(db.bibliography ? db.bibliography.trim() : ""),
+    component: String(db.component ? db.component.trim() : ""),
+    content: String(db.content ? db.content.trim() : ""),
+    createdAt: String(db.createdAt ? db.createdAt : DATE),
+    elaborated: Object(db.elaborated ? db.elaborated : {}),
+    id: String(db.id ? db.id : v4()),
+    facultyId: String(db.facultyId ? db.facultyId : ""),
+    index: String(db.index ? db.index.trim() : ""),
+    integrated: Object(db.integrated ? db.integrated : {}),
+    introduction: String(db.introduction ? db.introduction.trim() : ""),
+    isActive: Boolean(db.isActive === true ? true : false),
+    isLock: Boolean(db.isLock === true ? true : false),
+    programId: String(db.programId ? db.programId : ""),
+    revised: Object(db.revised ? db.revised : {}),
+    summary: String(db.summary ? db.summary.trim() : ""),
+    teacher: String(db.teacher ? db.teacher.trim() : ""),
+    title: String(db.title ? db.title.trim() : ""),
+    unit: String(db.unit ? db.unit.trim() : ""),
+    updatedAt: String(db.updatedAt ? db.updatedAt : DATE),
+  }
+}
+
+export const RefResource = (db: IResource) => {
+  return {
+    id: String(db.id ? db.id : v4()),
+    isActive: Boolean(db.isActive === true ? true : false),
+    teacher: String(db.teacher ? db.teacher.trim() : ""),
+    title: String(db.title ? db.title.trim() : ""),
+    unit: String(db.unit ? db.unit.trim() : ""),
+  }
+}
+
+export const Role = (db: IRole) => {
+  return {
+    createdAt: String(db.createdAt ? db.createdAt : DATE),
+    description: String(db.description ? db.description.trim() : ""),
+    id: String(db.id ? db.id : v4()),
+    isActive: Boolean(db.isActive === true ? true : false),
+    isLock: Boolean(db.isLock === true ? true : false),
+    name: String(db.name.trim()),
+    updatedAt: String(db.updatedAt ? db.updatedAt : DATE),
+  }
+}
+
+export const RefRole = (db: IRole) => {
+  return {
+    id: String(db.id ? db.id : v4()),
+    isActive: Boolean(db.isActive === true ? true : false),
+    name: String(db.name.trim()),
   }
 }
 

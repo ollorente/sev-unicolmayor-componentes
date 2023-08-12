236902ae7a74a818681def36acab4b5d97e38dd5
Óscar Llorente <llorente_oscar@hotmail.com>
2023-08-12 00:53:53 -0500

    Creando archivo

---
+++ src/components/UI/CheckComponent.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const props = defineProps({
+  check: {
+    type: Boolean,
+    default: false,
+  },
+  id: {
+    type: String,
+    default: "",
+  },
+  titleCheck: {
+    type: String,
+    default: "Activado",
+  },
+  titleUncheck: {
+    type: String,
+    default: "Desactivado",
+  },
+})
+</script>
+
+<template>
+  <div class="mb-3">
+    <div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" role="switch" :id="props.id" :checked="props.check" />
+      <label class="form-check-label" :for="props.id">
+        {{ props.check ? props.titleCheck : props.titleUncheck }}
+      </label>
+    </div>
+  </div>
+</template>
---
+++ src/components/UI/CheckedComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  date: {
+    type: String,
+    default: "",
+  },
+  id: {
+    type: String,
+    default: "",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div class="mb-2">
+    <label :for="props.id" class="form-label d-flex justify-content-between align-items-center fw-bold text-secondary">
+      <span class="text-dark"><slot></slot></span>
+      <span style="font-size: 0.8rem;">{{ props.date }}</span>
+    </label>
+    <input type="text" class="form-control" :id="props.id" :placeholder="props.title">
+  </div>
+</template>

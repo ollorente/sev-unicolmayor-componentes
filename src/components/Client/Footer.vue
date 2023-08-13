fd55b1b3ee70c0470fd9844b4fe29f37a1e3780b
Óscar Llorente <llorente_oscar@hotmail.com>
2023-08-13 18:09:23 -0500

    Ajustando diseño

--- src/components/Client/Footer.vue
+++ src/components/Client/Footer.vue
@@ -3,7 +3,7 @@
     <div class="container mx-auto">
       <div class="row">
         <div class="d-flex flex-column flex-lg-row">
-          <div class="col-12 text-center">
+          <div class="col-12 text-center d-flex flex-column flex-md-row">
             <span class="">Subdirección de Educación Virtual</span>
             <span class="px-2">© 2022 - {{ new Date().getFullYear() }}</span>
           </div>
